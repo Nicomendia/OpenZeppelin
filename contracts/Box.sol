7479d29a7020c3ddf187b21a150e3177e5a57fa8
Nico M <nico.mendia@gmail.com>
2021-12-10 15:00:57 -0400

    Adding OZ contracts by using inheritance. Deploying locally and interacting with scripts/index.js

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,25 +3,19 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private _value;
-    Auth private _auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
-    constructor() {
-        _auth = new Auth(msg.sender);
-    }
-
     // Stores a new value in the contract
-    function store(uint256 value) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(_auth.isAdministrator(msg.sender), "Unauthorized");
-        
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 value) public onlyOwner {
         console.log("Changing value from '%s' to '%s'", _value, value);
         _value = value;
         emit ValueChanged(value);
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,11 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const Box = await hre.ethers.getContractFactory("Box");
+  console.log('Deploying Box...');
+  const box = await Box.deploy();
+  await box.deployed();
+  console.log("Box deployed to:", box.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
