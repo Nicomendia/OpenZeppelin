640221120464104de439ebae6e74e0666b148424
Nicolas Mendia <nico.mendia@gmail.com>
2022-04-04 09:19:17 -0400

    test

--- test/Box.test.js
+++ test/Box.test.js
@@ -1,6 +1,8 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// tests
+
 // Import utilities from Test Helpers
 const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
